777a62b284c23621b3066ec6fe588e1a5cfeff47
Bas van der Hoek <bas@hoek.com>
2019-09-10 14:59:19 +0200

    working ubiquity kvstore, and handoff

--- Classroom Timer/AppDelegate.swift
+++ Classroom Timer/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Classroom Timer
-//
-//  Created by Bas van der Hoek on 09/09/2019.
-//  Copyright © 2019 Zilverline. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
@@ -19,28 +11,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     return true
   }
 
-  func applicationWillResignActive(_ application: UIApplication) {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+  func application(_ application: UIApplication, didFailToContinueUserActivityWithType userActivityType: String, error: Error) {
+    print("ios didFail \(userActivityType)")
   }
 
-  func applicationDidEnterBackground(_ application: UIApplication) {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+  func application(_ application: UIApplication, didUpdate userActivity: NSUserActivity) {
+    print("ios didUpdate \(userActivity)")
   }
 
-  func applicationWillEnterForeground(_ application: UIApplication) {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+  func application(_ application: UIApplication, willContinueUserActivityWithType userActivityType: String) -> Bool {
+    print("ios willContinue \(userActivityType)")
+    return false
   }
 
-  func applicationDidBecomeActive(_ application: UIApplication) {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-  }
+  func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
 
-  func applicationWillTerminate(_ application: UIApplication) {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    print("ios continue \(userActivity)")
+    return true
   }
-
 
 }
 
--- Classroom Timer/ViewController.swift
+++ Classroom Timer/ViewController.swift
@@ -1,20 +1,42 @@
-//
-//  ViewController.swift
-//  Classroom Timer
-//
-//  Created by Bas van der Hoek on 09/09/2019.
-//  Copyright © 2019 Zilverline. All rights reserved.
-//
-
 import UIKit
+
+let ActivityType = "com.zilverline.classroom.timer-running"
 
 class ViewController: UIViewController {
 
+  var keyStore: NSUbiquitousKeyValueStore?
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    keyStore = NSUbiquitousKeyValueStore()
+
+    NotificationCenter.default.addObserver(self, selector: #selector(onUbiquitousKeyValueStoreDidChangeExternally(notification:)), name: NSUbiquitousKeyValueStore.didChangeExternallyNotification, object: keyStore)
   }
 
+  @objc func onUbiquitousKeyValueStoreDidChangeExternally(notification: Notification) {
+    print("changed \(keyStore?.object(forKey: "timer"))")
+  }
+
+  @IBAction func activity(_ sender: Any) {
+    startActivity()
+  }
+
+  func startActivity() {
+    let activity = NSUserActivity(activityType: ActivityType)
+    activity.title = "Running Timer"
+    self.userActivity = activity;
+
+    keyStore?.set(Date(), forKey: "timer")
+    keyStore?.synchronize()
+    print(keyStore?.object(forKey: "timer"))
+  }
+
+  override func updateUserActivityState(_ activity: NSUserActivity) {
+    super.updateUserActivityState(activity)
+
+    activity.addUserInfoEntries(from: ["Time": NSDate()])
+  }
 
 }
 
